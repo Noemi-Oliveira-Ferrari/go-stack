39d161abc96a2539e67ece36f90b8672036e7dcd
Noemi Ferrari <noemiferrari287@gmail.com>
2020-03-25 12:43:28 -0300

    Início da estilização da tela e consumo da api do github

--- modulo05/src/App.js
+++ modulo05/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Routes from './routes.js';
+import GlobalStyle from './styles/global';
 
 
 function App() {
   return (
-        <Routes>
 
-        </Routes>
+    <>
+        <Routes/>
+        <GlobalStyle/>
+    </>
     );
 }
 
--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Main(){
+import { FaGithubAlt, FaPlus} from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
-    return <h1>MAIN</h1>
+import api from '../../services/api';
 
-}
+export default class Main extends Component{
 
-export default Main;
+    state = {
+        newRepo: '',
+    }
+
+    handleInputChange = e =>{
+        this.setState({newRepo: e.target.value});
+    }
+
+    handleSubmit = async e =>{
+        e.preventDefault();
+        const { newRepo } = this.state;
+        const response = await api.get(`/repos/${newRepo}`);
+        console.log(response.data);
+
+
+    }
+    render(){
+
+        const { newRepo } = this.state
+
+        return(
+            <Container>
+                <h1>
+                    <FaGithubAlt/>
+                    Repositórios
+                </h1>
+                <Form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Adicionar repositório"
+                        value={newRepo}
+                        onChange={this.handleInputChange}
+                    />
+                    <SubmitButton>
+                        <FaPlus color="#fff" size={14}/>
+                    </SubmitButton>
+                </Form>
+
+            </Container>
+        )}
+    }
